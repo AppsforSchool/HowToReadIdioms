f17908777d5847cba2980b467502e80f66fd86fe
AppsforSchool <sugaku496@icloud.com>
2026-07-08 20:04:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // detail buttons
 let detailSelectionState = new Set(["on_on", "kun_kun", "on_kun", "kun_on", "jukujikun", "multi"]);
-let allQuestionCount = 0;
+let allQuestionCount = 79;
 let detailButtons, allQuestion;
 document.addEventListener('DOMContentLoaded', () => {
   detailButtons = document.querySelectorAll('.detail-button');
